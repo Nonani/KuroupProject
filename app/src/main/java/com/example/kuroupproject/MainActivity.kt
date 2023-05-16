47f038e9731d133de96756a4ed067fa324b03c15
cjw4458 <cjw1234577@gmail.com>
2023-05-16 10:29:23 +0900

    DB Preferences: insert 예제 구현

--- app/src/main/java/com/example/kuroupproject/MainActivity.kt
+++ app/src/main/java/com/example/kuroupproject/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.kuroupproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kuroupproject.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.apply {
+            button.setOnClickListener {
+                val db = FirebaseFirestore.getInstance()
+                val user = hashMapOf(
+                    "id" to id.text.toString(),
+                    "pwd" to pwd.text.toString(),
+                )
+
+// Add a new document with a generated ID
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d("Success", "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("Error", "Error adding document", e)
+                    }
+            }
+
+        }
+
+
     }
 }
