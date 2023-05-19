4ff89bbb6332eba2ff4dc1cf4016da87ed4e2186
cjw4458 <cjw1234577@gmail.com>
2023-05-19 17:39:32 +0900

    feat: Firebase Authentication 연동 회원가입/로그인 구현

--- app/src/main/java/com/example/kuroupproject/MainActivity.kt
+++ app/src/main/java/com/example/kuroupproject/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.kuroupproject
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kuroupproject.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initLayout()
@@ -19,25 +27,65 @@ class MainActivity : AppCompatActivity() {
     private fun initLayout() {
         binding.apply {
             registerButton.setOnClickListener {
-                val db = FirebaseFirestore.getInstance()
-                val user = hashMapOf(
-                    "id" to id.text.toString(),
-                    "pwd" to pwd.text.toString(),
-                )
+                val email = id.text.toString()
+                val pwd = pwd.text.toString()
 
-// Add a new document with a generated ID
-                db.collection("users")
-                    .add(user)
-                    .addOnSuccessListener { documentReference ->
-                        Log.d("Success", "DocumentSnapshot added with ID: ${documentReference.id}")
+
+                auth.createUserWithEmailAndPassword(email, pwd)
+                    .addOnCompleteListener{ result ->
+                        if (result.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            val user = auth.currentUser
+                            val db = FirebaseFirestore.getInstance()
+                            val userData = hashMapOf(
+                                "id" to email,
+                                "pwd" to pwd,
+                            )
+                        // Add a new document with a generated ID
+                            db.collection("users").document(user!!.uid)
+                                .set(userData)
+                                .addOnSuccessListener { documentReference ->
+                                    Toast.makeText(this@MainActivity, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener { e ->
+                                    Log.w("Error", "Error adding document", e)
+                                }
+//                            updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this@MainActivity, result.exception.toString(), Toast.LENGTH_SHORT).show()
+//                            updateUI(null)
+                        }
                     }
-                    .addOnFailureListener { e ->
-                        Log.w("Error", "Error adding document", e)
-                    }
+            }
+            loginButton.setOnClickListener {
+                val email = id.text.toString()
+                val pwd = pwd.text.toString()
+                if (email.isNotEmpty() && pwd.isNotEmpty()) {
+                    auth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener { result ->
+                            if (result.isSuccessful) {
+                                Log.d("success", "Error adding document")
+                                var intent = Intent(this@MainActivity, HomeActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(this@MainActivity, result.exception.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+
             }
 
         }
+    }
 
-
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            currentUser.reload()
+        }
     }
 }
