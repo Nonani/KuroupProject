c35b609807adf6960dbc093f40fbc47c7d652d7c
davinida <davin0925@konkuk.ac.kr>
2023-05-21 20:22:11 +0900

    feat: 상세화면 구현 중

---
+++ app/src/main/java/com/example/kuroupproject/ContestDetailData.kt
@@ -0,0 +1,12 @@
+package com.example.kuroupproject
+
+import android.media.Image
+
+data class ContestDetailData(
+    var img: Image,
+    var apply: String,
+    var supporting: String,
+    var period: String,
+    var reword: String,
+    var homepage: String,
+)
