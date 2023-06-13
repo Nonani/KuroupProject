fcc16de0a11ad18ac2b2156d36af28bc5b18e468
cjw4458 <cjw1234577@gmail.com>
2023-06-14 02:57:22 +0900

    feats: 내가 만든 팀 기능 구현 완료

--- app/src/main/java/com/example/kuroupproject/datas/UserData.kt
+++ app/src/main/java/com/example/kuroupproject/datas/UserData.kt
@@ -1,11 +1,12 @@
 package com.example.kuroupproject.datas
 
-data class UserData(val gender:String,
+data class UserData(val uid:String,
+                    val gender:String,
                     val language:String,
                     val location:String,
                     val mainPart:String,
                     val name:String,
                     val onelineExplain:String,
                     val phonenum:String,
-                    val scrap:MutableList<Map<String,String>>, ) {
+                    val scrap:MutableList<Map<String,String>> ) {
 }
