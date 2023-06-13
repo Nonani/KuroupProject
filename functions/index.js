a77e25b592c5c918e36ed498283920a30880d904
cjw4458 <cjw1234577@gmail.com>
2023-06-13 13:07:51 +0900

    feat: api 구현 추가

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,26 @@
 const functions = require('firebase-functions');
 const express = require('express');
+var admin = require("firebase-admin");
+const firestore = require("firebase-admin/firestore");
+
+var serviceAccount = require("./kuroup-project-firebase-adminsdk-2nc6t-e7c247e774.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const database = firestore.getFirestore();
 const contestRouter = require('./contest');
+const teamRouter = require('./team');
 const app = express();
 app.use('/contest', contestRouter);
+app.use('/team', teamRouter);
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }))
-app.get('/', (req, res) => {
-  res.json({
-    success: true,
-  });
+app.get('/', async (req, res) => {
+
+  res.send(tmp)
 });
 
 
@@ -19,5 +29,5 @@ app.listen(port, () => {
   console.log(`server is listening at localhost:${process.env.PORT}`);
 });
 
-// // test prefix를 가지는 요청을 express 라우터로 전달
+// test prefix를 가지는 요청을 express 라우터로 전달
 // exports.app = functions.https.onRequest(app);
---
+++ functions/team.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const firestore = require("firebase-admin/firestore");
+const router = express.Router();
+router.use(express.json());
+const database = firestore.getFirestore();
+
+
+router.post('/supporting', async (req, res) => {
+    try {
+        const { uid } = req.body;
+        console.log(uid);
+        const map = {};
+        const postsRef = database.collection('posts');
+        const querySnapshot1 = await postsRef
+            .where('members_info', 'array-contains', { "uid": uid, "state": "waiting" })
+            .get();
+        const waiting_list = []
+        querySnapshot1.forEach(doc => {
+            waiting_list.push(doc.data());
+        });
+        const accepted_list = []
+        const querySnapshot2 = await postsRef
+            .where('members_info', 'array-contains', { "uid": uid, "state": "accepted" })
+            .get();
+        querySnapshot2.forEach(doc => {
+            accepted_list.push(doc.data());
+        });
+        map["accepted_list"] = accepted_list;
+        map["waiting_list"] = waiting_list;
+        res.send(map);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+
+});
+router.post('/created', async (req, res) => {
+
+    try {
+        const { uid } = req.body;
+        console.log(uid);
+        const collectionRef = database.collection('posts');
+        const querySnapshot1 = await collectionRef.where('leader_uid', '==', uid).get();
+
+        const map = {};
+        const accepted_list = [];
+        const waiting_list = [];
+        const accepted_users = [];
+        const waiting_users = [];
+
+        querySnapshot1.forEach(doc => {
+            doc.data().members_info.forEach(member => {
+                if (member.state == "accepted") {
+                    accepted_list.push(member.uid);
+
+                } else if (member.state == "waiting") {
+                    waiting_list.push(member.uid);
+                }
+            });
+        });
+
+        console.log(waiting_list);
+        console.log(accepted_list);
+        const usersRef = database.collection('users');
+
+        if (waiting_list.length != 0) {
+            for (const uid of waiting_list) {
+                const doc = await usersRef.doc(uid).get();
+                if (doc.exists) {
+                    accepted_users.push(doc.data());
+                } else {
+                    console.log(`User with uid ${uid} does not exist.`);
+                }
+            }
+        }
+
+        if (accepted_list.length != 0) {
+            for (const uid of accepted_list) {
+                const doc = await usersRef.doc(uid).get();
+                if (doc.exists) {
+                    accepted_users.push(doc.data());
+                } else {
+                    console.log(`User with uid ${uid} does not exist.`);
+                }
+            }
+        }
+        map["waiting_users"] = waiting_users;
+        map["accepted_users"] = accepted_users;
+        res.send(map);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = router;
